3bcaa85f85c77d48e039b42b3df6692d759aa388
Aleksa Ognjanović <alexa.ognjanovic@protonmail.com>
2026-07-10 15:13:29 +0200

    feat: make it so that CSV follows header order

--- src/main/java/io/github/GrbavaCigla/io/CSV.java
+++ src/main/java/io/github/GrbavaCigla/io/CSV.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class CSV<T> implements Importer<T>, Exporter<T> {
@@ -27,11 +28,22 @@ public abstract class CSV<T> implements Importer<T>, Exporter<T> {
     @Override
     public List<T> load(BufferedReader rd) throws IOException {
         List<T> result = new ArrayList<>();
-        rd.readLine(); // skip header
+        List<String> columns = Arrays.asList(getColumns());
+        List<String> header = Arrays.stream(rd.readLine().split(",")).map(s -> s.trim()).toList();
         String line;
         while ((line = rd.readLine()) != null) {
             if (line.isBlank()) continue;
-            result.add(fromRow(line.split(",")));
+
+            String[] item = new String[columns.size()];
+            String[] fields = line.split(",");
+            for (int i = 0; i < fields.length; i++) {
+                int index = columns.indexOf(header.get(i));
+                if (index == -1)
+                    throw new IOException("Unknown column: " + header.get(i));
+                item[index] = fields[i].trim();
+            }
+
+            result.add(fromRow(item));
         }
         return result;
     }
